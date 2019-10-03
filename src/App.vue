bae11bb6c0edbb4988c5562f0d05212e11eef675
rinne-grid <Rinne.Grids@gmail.com>
2019-10-03 22:26:23 +0900

    ADD validate

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+<!--    <div id="nav">-->
+<!--      <router-link to="/"></router-link>-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+<!--    <router-view/>-->
   </div>
 </template>
-
+<script>
+  console.log("app.vue-start");
+  console.log(app);
+  app.ondragstart = (e) => {
+    e.preventDefault();
+    console.log("dragstart-");
+  };
+  app.ondragover = (e) => {
+    e.preventDefault();
+    console.log("dragover-");
+  };
+  app.ondragenter = (e) => {
+    e.preventDefault();
+  };
+  app.ondrop = (e) => {
+    e.preventDefault();
+  };
+</script>
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+    width: 100%;
+    height: 1368px;
+    max-height: 1368px;
+    background-color: green;
+    cursor: pointer;
 }
 </style>
---
+++ src/views/ViewerContext.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="area">
+        a
+    </div>
+</template>
+
+<script>
+    import { ipcRenderer, ipcMain } from 'electron'
+    export default {
+        name: 'ViewerContext',
+
+        mounted() {
+            console.log(app);
+            app.ondragstart = (event) => {
+                event.preventDefault();
+                console.log(event);
+                ipcRenderer.send("ondragstart", "test");
+            };
+        },
+    };
+</script>
+
+<style scoped type="scss">
+#area {
+    width: 100%;
+    height: 100%;
+    background-color: green;
+}
+</style>
