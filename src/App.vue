8877c0bac825341e8412bf9e924bbd0f191610f7
rinne-grid <Rinne.Grids@gmail.com>
2019-10-23 23:07:27 +0900

    ADD upload file

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app">
+  <div id="app" @dragenter="doDragEnd">
+    <p>ファイルを選択</p>
+    <input type="file" @change="setFilePath">
+
 <!--    <div id="nav">-->
 <!--      <router-link to="/"></router-link>-->
 <!--      <router-link to="/">Home</router-link> |-->
@@ -9,22 +12,33 @@
   </div>
 </template>
 <script>
-  console.log("app.vue-start");
-  console.log(app);
-  app.ondragstart = (e) => {
-    e.preventDefault();
-    console.log("dragstart-");
-  };
-  app.ondragover = (e) => {
-    e.preventDefault();
-    console.log("dragover-");
-  };
-  app.ondragenter = (e) => {
-    e.preventDefault();
-  };
-  app.ondrop = (e) => {
-    e.preventDefault();
-  };
+  export default {
+    methods: {
+      doDragEnd: (e) => {
+        console.log(e);
+      },
+      setFilePath: (e) => {
+        // this.filePath = e.target.files;
+        console.log(e.target.files)
+      }
+    }
+  }
+  // console.log("app.vue-start");
+  // console.log(app);
+  // app.ondragstart = (e) => {
+  //   e.preventDefault();
+  //   console.log("dragstart-");
+  // };
+  // app.ondragover = (e) => {
+  //   e.preventDefault();
+  //   console.log("dragover-");
+  // };
+  // app.ondragenter = (e) => {
+  //   e.preventDefault();
+  // };
+  // app.ondrop = (e) => {
+  //   e.preventDefault();
+  // };
 </script>
 <style lang="scss">
 #app {
